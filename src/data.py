73025960721b2cd5cd6cc20818fa227e66044fe2
Michael Dereszynski <mlderes@hotmail.com>
2022-03-14 00:25:17 +0000

    Begin work on the unsupervised notebook

--- src/data.py
+++ src/data.py
@@ -8,8 +8,26 @@ TRUE_VALUES = ["Yes", "yes", "y", "Y"]
 
 
 def load_data(fileName: str, **kwargs) -> pd.DataFrame:
-    return pd.read_csv(DATA_DIR / f"{fileName}.csv")
+    return pd.read_csv(DATA_DIR / f"{fileName}.csv", **kwargs)
 
 
 def load_excel(fileName: str, **kwargs) -> pd.DataFrame:
     return pd.read_excel(DATA_DIR / f"{fileName}.xlsx", **kwargs)
+
+
+def normalize(data:pd.DataFrame, columns, drop_old=False)-> pd.DataFrame:
+    for c in columns:
+        new_column_name = f"{c}_NORM"
+        data[new_column_name] = (data[c] - data[c].min()) / (data[c].max() - data[c].min())
+    if drop_old:
+        data.drop(columns=columns,inplace=True)
+    return data
+
+
+def standardize(data:pd.DataFrame, columns, drop_old=False)-> pd.DataFrame:
+    for c in columns:
+        new_column_name = f"{c}_STD"
+        data[new_column_name] = (data[c] - data[c].mean()) / data[c].std()
+    if drop_old:
+        data.drop(columns=columns,inplace=True)
+    return data
